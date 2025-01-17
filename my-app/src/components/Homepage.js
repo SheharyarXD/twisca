4f66a4a6e1eca433ce06c1d59b42877ba9c2efbc
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-01-17 18:21:21 +0500

    Header

--- my-app/src/components/Homepage.js
+++ my-app/src/components/Homepage.js
@@ -16,6 +16,21 @@ return(
         <button className="bg-[#8B024B] text-white px-[3vw] py-2 rounded-[1vw] mt-[3vh] text-[1.2rem]">Order Your Perfect Gift<i class="fa-solid fa-arrow-right pl-[1vw]"></i></button>
     </div>
     </div>
+    <div className="h-screen text-center py-[5vh]">
+        <div className="font-bold text-[3.5rem] leading-tight">Our Most-Loved Picks,Just for <br />You!</div>
+        <p className="text-gray-600 pt-[4vh]">These are the gifts everyone’s talking about! From timeless classics to trendy must-haves, explore our bestsellers <br /> that bring smiles and unforgettable moments.</p>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </div>
     </>
 );
 }
