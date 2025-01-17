fb8cbe78db376b2892fb2378ee5b55312347f6a8
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-01-17 19:50:06 +0500

    Header

--- my-app/src/components/Homepage.js
+++ my-app/src/components/Homepage.js
@@ -39,7 +39,7 @@ return(
     disableOnInteraction: false, 
   }}
   modules={[Navigation,Autoplay]}
-  className="mySwiper mt-[5vh] w-[90vw]"
+  className="mySwiper mt-[5vh] w-[90vw] relative"
   slidesPerView="auto" // Allow slides to take up only their required space
   spaceBetween={25}
 >
@@ -52,8 +52,8 @@ return(
   <SwiperSlide className="max-w-[250px]"><LovedPorducts /></SwiperSlide>
   <SwiperSlide className="max-w-[250px]"><LovedPorducts /></SwiperSlide>
   <SwiperSlide className="max-w-[250px]"><LovedPorducts /></SwiperSlide>
-  <button className="custom-prev">Prev</button>
-  <button className="custom-next"><i class="fa-solid fa-arrow-right pl-[1vw]"></i></button>
+  <button className="custom-prev text-[#8B024B] border absolute border-[#8B024B] text-[1.5rem] rounded-full bg-white left-[4vw] top-[18vh] z-50"><i className="fa-solid fa-arrow-left py-4 px-4"></i></button>
+  <button className="custom-next text-[#8B024B] border absolute border-[#8B024B] text-[1.5rem] rounded-full bg-white right-[4vw]  top-[18vh] z-50"><i className="fa-solid fa-arrow-right py-4 px-4"></i></button>
 </Swiper>
     </div>
     </>
