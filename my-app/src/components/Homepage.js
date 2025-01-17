9c6cd8310143c46804077f6cdaf81372504df722
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-01-17 18:23:52 +0500

    2nd section

--- my-app/src/components/Homepage.js
+++ my-app/src/components/Homepage.js
@@ -1,5 +1,12 @@
 import React from "react";
 import Header from './header';
+// Import Swiper styles
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+
+
 const HomePage=()=>{
     const divStyle = {
         backgroundColor: '#D52260',
