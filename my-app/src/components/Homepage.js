c2fface25e7936193fc519e5743d4af35fd2b7fd
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-01-18 16:55:02 +0500

    2 sections More

--- my-app/src/components/Homepage.js
+++ my-app/src/components/Homepage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from './header';
 import LovedPorducts from "./LovedProducts";
+import SurprisedProducts from "./SurprisesProducts";
 // Import Swiper styles
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -17,6 +18,7 @@ const HomePage=()=>{
       };
 return(
     <>
+    {/* Main Section */}
     <div className="h-screen overflow-hidden">
     <Header/>
     <div className="w-full h-[88vh] text-center items-center flex flex-col " style={divStyle}>
@@ -24,7 +26,9 @@ return(
         <button className="bg-[#8B024B] text-white px-[3vw] py-2 rounded-[1vw] mt-[3vh] text-[1.2rem]">Order Your Perfect Gift<i class="fa-solid fa-arrow-right pl-[1vw]"></i></button>
     </div>
     </div>
-    <div className="h-screen text-center pt-[10vh]">
+    {/* Loved Porducts Section */}
+    <section>
+          <div className="h-screen text-center pt-[10vh] overflow-hidden">
         <div className="font-bold text-[3rem] leading-tight">Our Most-Loved Picks,Just for <br />You!</div>
         <p className="text-gray-600 pt-[4vh]">These are the gifts everyone’s talking about! From timeless classics to trendy must-haves, explore our bestsellers <br /> that bring smiles and unforgettable moments.</p>
         <Swiper
@@ -56,6 +60,30 @@ return(
   <button className="custom-next text-[#8B024B] border absolute border-[#8B024B] text-[1.5rem] rounded-full bg-white right-[4vw]  top-[18vh] z-50"><i className="fa-solid fa-arrow-right py-4 px-4"></i></button>
 </Swiper>
     </div>
+    </section>
+    {/* Surprise Products Section */}
+    <section>
+    <div className="h-screen text-center pt-[10vh] relative overflow-hidden">
+        <img className="absolute top-[15vh] w-auto h-[11vh] left-[11vw]" src="./Images/star.png" alt="" />
+        <img className="absolute w-auto h-[20vh] top-[8vh] right-[10vw]" src="./Images/love.png" alt="" />
+        <div className="font-bold text-[3rem] leading-tight">Celebrate the Season of Love <br />with Surprises!</div>
+        <div class="flex flex-row items-center justify-center h-[3.7vw] w-[20vw] mx-auto mt-[6vh] text-[#8B024B]">
+              <button id="beforeButton" class="text-[1.3vw] border-2 border-[#8B024B] hover:bg-[#8B024B] rounded-l-[40px] focus:bg-[#8B024B] focus:text-white hover:text-white h-full w-1/2 cursor-default">👔 For Him</button>
+<button id="afterButton" class="text-[1.3vw] hover:bg-[#8B024B] border-2 border-[#8B024B] rounded-r-[40px] focus:bg-[#8B024B] focus:text-white hover:text-white h-full w-1/2 cursor-default">👗 For Her</button>
+
+            </div>
+        <div className="flex flex-row justify-between mx-[13vw] py-[5vh]">
+        <SurprisedProducts/>
+        <SurprisedProducts/>
+        <SurprisedProducts/>
+        </div>
+    </div>
+    {/* Motivation Line */}
+    <section>
+      <div className="h-screen text-center pt-[35vh] relative overflow-hidden font-bold leading-tight text-[#CFCFCF] text-[3.5rem] ">
+        <span className="text-[#8B024B] "> Find 🎁</span>, <span className="text-[#8B024B] "> customize</span> ✍️, and  <span className="text-[#8B024B] "> send</span> 💌 the <br /> perfect gift—all in one delightful <br /> place.</div>
+    </section>
+    </section>
     </>
 );
 }
---
+++ my-app/src/components/SurprisesProducts.js
@@ -0,0 +1,22 @@
+import React from "react";
+const SurprisedProducts=()=>{
+return(
+    <div className="max-h-fit bg-white shadow-md w-[23vw] rounded-[1vw]">
+    <div className="h-fit overflow-hidden w-full object-cover relative ">
+    <img src="./Images/sampleImage2.png" className="object-cover w-full h-auto" alt="" />
+    </div>
+    <div className="flex flex-row text-[1.1rem] font-bold px-2 pt-[2vh]">
+        <div className=" text-start">Classic Accessories</div>
+    </div>
+    <div className="flex flex-row text-[0.7rem] px-2 pt-[0.8vh]">
+        <div className=" text-start text-gray-600 font-semibold">Elegant essentials he’ll love to carry every day</div>
+    </div>
+   
+    <div className="flex flex-row items-center justify-center pt-[1vh] pb-2">
+    <button className="bg-[#8B024B] text-white w-[80%] text-[0.8rem] py-2 rounded-[0.5vw] ">Explore All</button>
+    <button className="border-[#8B024B] border text-white ml-[0.5vw] px-[0.8vw] py-[0.9vw] rounded-full "><img src="./Images/Union.png" alt="" /></button>
+    </div>
+</div>
+);
+}
+export default SurprisedProducts;
