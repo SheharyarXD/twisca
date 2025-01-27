e95cae40b8d0ea44a2e8d12c0061d9a71131eac3
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-01-27 17:50:45 +0500

    In progress

--- my-app/src/components/cart.js
+++ my-app/src/components/cart.js
@@ -9,21 +9,18 @@ const Cart=()=>{
         <div className="flex flex-row items-center">
             {/* left section */}
             <div className="w-[60vw] px-[5vw]">
-            <div className="rounded-xl shadow-md shadow-gray-600 mt-[5vh] h-[70vh] overflow-y-scroll">
+            <div className="rounded-xl shadow-md shadow-gray-600 mt-[5vh] h-[70vh] ">
                 {/* header of table */}
                 <div className="flex text-[1.1rem] sticky top-0 z-[100] flex-row items-center bg-[#8B024B] justify-center  text-white py-[2vh] overflow-hidden rounded-t-xl" >
-                    <div className="w-[45%]">Product</div>
-                    <div className="w-[35%] flex">
-                    <div className="px-[9%]">Price</div>
-                    <div className="px-[2vw]">Quantity</div>
-                    <div className="px-[1vw]">Total</div>
+                    <div className="w-[23vw]">Product</div>
+                    <div className="w-[18vw] flex">
+                    <div className="w-[6vw] text-center">Price</div>
+                    <div className=" text-center w-[10vw]">Quantity</div>
+                    <div className="w-[2vw]">Total</div>
                     </div>
                 </div>
                 {/* products in cart */}
                 <div className=" flex flex-col">
-                <CartList/>
-                <CartList/>
-                <CartList/>
                 <CartList/>
                 <CartList/>
                 <CartList/>
