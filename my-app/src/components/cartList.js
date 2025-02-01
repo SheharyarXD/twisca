bf06c46f6d9d058273a863e338e2323a15702a71
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-02-02 01:50:32 +0500

    Backend Linking

--- my-app/src/components/cartList.js
+++ my-app/src/components/cartList.js
@@ -68,7 +68,7 @@ const CartList=( {
               </button>
             </div>
           </div>
-          <div className="w-[6vw] flex text-[#949494]">$ <p>{TotalPrice}</p></div>
+          <div className="w-[6vw] flex text-[#949494]">$ <p>{(originalPrice*quantity)}</p></div>
         </div>);
 }
 export default CartList;
