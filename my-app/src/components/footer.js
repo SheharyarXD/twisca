9dbde65390b0c73e11fd13e867b8a766697c5457
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-02-04 17:54:52 +0500

    UI Fixing

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -1,15 +1,15 @@
 import React from "react";
 const FooterPage=()=>{
 return(   <footer className="text-white bg-[#8B024B] text-[0.8rem] rounded-t-[3rem] px-[5vw] mt-[5vh]" style={{boxShadow: '0 -10px 100px rgba(227, 32, 136, 0.5)'}}>
-    <div className="upperSection flex flex-row pt-[6vh] min-h-fit pb-[15vh]">
+    <div className="upperSection flex flex-col sm:flex-row pt-[6vh] min-h-fit pb-[15vh]">
       <div className="w-[16%] flex flex-row items-center h-fit">
         <img src="./Images/logoWhite.png" className="h-[4.5vh] w-auto mr-[0.4vw]" alt="" />
         <div className="text-[1.5rem] font-semibold">
            twisca
           </div>
           </div>
-      <div className="w-[70%] flex flex-row justify-around">
-        <div className="flex flex-col w-[10%]">
+      <div className="sm:w-[70%] pt-[5vh] sm:pt-0 flex flex-row justify-around">
+        <div className="flex flex-col sm:w-[10%]">
           <a className="pb-3" href="">Home</a>
           <a className="pb-3" href="">Products</a>
           <a className="pb-3" href="">Occasion</a>
@@ -17,12 +17,12 @@ return(   <footer className="text-white bg-[#8B024B] text-[0.8rem] rounded-t-[3r
           <a className="pb-3" href="">Contact</a>
           <a className="pb-3" href="">Newsletter</a>
         </div>
-        <div  className="flex flex-col w-[10%]">
+        <div  className="flex flex-col sm:w-[10%]">
           <a className="pb-3" href="">Information</a>
           <a className="pb-3" href="">Data Attributes</a>
           <a className="pb-3" href="">Support</a>
         </div>
-        <div  className="flex flex-col w-[10%]">
+        <div  className="flex flex-col sm:w-[10%]">
           <a className="pb-3" href="">Company</a>
           <a className="pb-3" href="">About Us</a>
           <a className="pb-3" href="">Careers</a>
@@ -33,7 +33,7 @@ return(   <footer className="text-white bg-[#8B024B] text-[0.8rem] rounded-t-[3r
     </div>
     <div className="baseline flex flex-col ">
       <div className="h-[0.09px] bg-gray-400"></div>
-      <div className="py-[2vh] flex justify-between w-[50%]">
+      <div className="py-[2vh] md:text-base text-[0.6rem] flex justify-between lg:w-[50%]">
      <a href="">Trust Center</a> 
      <a href="">Privacy Policy</a> 
      <a href="">Status</a> 
