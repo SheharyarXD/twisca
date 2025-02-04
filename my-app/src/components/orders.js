661a0d2411bbea096fcf4c421a47ca7f77bea936
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-02-04 18:33:40 +0500

    UI Fixing

--- my-app/src/components/orders.js
+++ my-app/src/components/orders.js
@@ -6,7 +6,7 @@ const OrderDiv = ({ image, title, category, price, rating }) => {
             <div className="max-w-[30%]">
                 <img className="h-full w-auto" src={image} alt={title} />
             </div>
-            <div className="flex text-sm px-3 font-bold flex-col w-[60%]">
+            <div className="flex text-sm px-3 font-bold flex-col min-w-[60%]">
                 <div>{title}</div>
                 <div>Category: <span className="font-semibold">{category}</span></div>
                 <div className="text-[#8B024B] font-semibold text-[1rem]">${price}</div>
