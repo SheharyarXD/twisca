f06fc727660b25f9713de6762811699f5d523319
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-02-05 14:01:17 +0500

    UI Fixing

--- my-app/src/components/comment.js
+++ my-app/src/components/comment.js
@@ -1,40 +1,46 @@
 import React from "react";
-const CommentsTemplate=({ name, date, rating, comment, likes, dislikes, profileImage })=>{
-    return( 
-        
-           <div className="p-4 border-[#7E7E80] border-2 min-w-full h-[150px] rounded-lg flex flex-row px-6 min-h-fit">
-        <div className="w-[90px]">
-          <img
-            className="h-11 mr-[1vw] w-auto rounded-full"
-            src={profileImage}
-            alt=""
-          />
-        </div>
-        <div className="px-3">
-          <div className="flex flex-row justify-between items-center ">
-            <div className="flex flex-row items-center">
-              <div className="font-semibold text-lg">{name}</div> 
-              <div className="text-xs pl-[2.5vw] text-gray-400 font-semibold">{date}</div>
-            </div>
-            <div className="flex flex-row px-2 text-xs items-center pt-[1vh] text-gray-500">
-              <img
-                src="../Images/ri-star-fill.png"
-                className="pr-1"
-                alt=""
-              />
-              <p>
-                <span className="pr-3 text-black text-[1rem] font-bold">{rating}</span>
-              </p>
-            </div>
+
+const CommentsTemplate = ({ name, date, rating, comment, likes, dislikes, profileImage }) => {
+  return (
+    <div className="p-4 border-[#7E7E80] border-2 w-full max-w-lg sm:max-w-full md:max-w-full md:min-w-full rounded-lg flex flex-wrap md:flex-nowrap px-6">
+      {/* Profile Image */}
+      <div className="w-16 flex-shrink-0">
+        <img className="h-11 w-11 rounded-full" src={profileImage} alt="User Profile" />
+      </div>
+
+      {/* Comment Details */}
+      <div className="flex-1 px-3">
+        <div className="flex flex-wrap md:flex-nowrap justify-between items-center">
+          {/* Name & Date */}
+          <div className="flex flex-wrap items-center">
+            <div className="font-semibold text-lg">{name}</div>
+            <div className="text-xs md:pl-4 text-gray-400 font-semibold">{date}</div>
           </div>
-          <div className="text-[#7E7E80] pt-[1vh]">
-           {comment}
-          </div>
-          <div className="flex flex-row items-center pt-[10px]">
-            <div className="px-2"><i className="fa-solid px-2 fa-thumbs-up"></i>{likes}</div>
-            <div className="px-2"><i className="fa-solid px-2 fa-thumbs-down"></i>{dislikes}</div>
+
+          {/* Rating */}
+          <div className="flex items-center text-xs text-gray-500 pt-2 md:pt-0">
+            <img src="../Images/ri-star-fill.png" className="pr-1 w-4 h-4" alt="Star" />
+            <p className="text-black text-[1rem] font-bold">{rating}</p>
           </div>
         </div>
-      </div>);
-}
+
+        {/* Comment Text */}
+        <div className="text-[#7E7E80] pt-2">{comment}</div>
+
+        {/* Like & Dislike Buttons */}
+        <div className="flex space-x-4 pt-3">
+          <div className="flex items-center space-x-1 text-gray-600">
+            <i className="fa-solid fa-thumbs-up"></i>
+            <span>{likes}</span>
+          </div>
+          <div className="flex items-center space-x-1 text-gray-600">
+            <i className="fa-solid fa-thumbs-down"></i>
+            <span>{dislikes}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default CommentsTemplate;
