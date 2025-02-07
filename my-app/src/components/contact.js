e063d43bb37b1457df8032571ceb0b2293b3a428
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-02-07 19:50:32 +0500

    Fixing

--- my-app/src/components/contact.js
+++ my-app/src/components/contact.js
@@ -91,10 +91,11 @@ const Contact=()=>{
 
   
       <div className="w-full lg:w-1/2 hidden lg:block relative min-h-full md:min-h-0 ">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13596.04142745665!2d74.33444081659832!3d31.57876385488925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39191b98b46808fb%3A0xb97ac7ef65a2d0a2!2sSincSol!5e0!3m2!1sen!2s!4v1734690540382!5m2!1sen!2s"
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d217759.55800898257!2d74.16958105977912!3d31.48312759108344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39190483e58107d9%3A0xc23abe6ccc7e2462!2sLahore%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1738939779938!5m2!1sen!2s" 
          className="w-full h-full absolute"
          style={{border:0}} 
          allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        
       </div>
     </div>
   </div>
